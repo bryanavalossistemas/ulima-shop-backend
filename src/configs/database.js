b0c353f034d281284865a99334bd41f27b4b9add
bryanavalossistemas <bryanavalossistemas@gmail.com>
2024-07-03 21:42:07 -0500

    second commit

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("ulima-shop", "postgres", "Password123", {
-  host: "servidor-de-abigail.postgres.database.azure.com",
+  host: "ulima-shop.postgres.database.azure.com",
   port: 5432,
   dialect: "postgres",
   dialectOptions: {
